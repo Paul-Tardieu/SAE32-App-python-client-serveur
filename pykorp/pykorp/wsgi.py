8f70b2a6128da2eeb976d8c193f7485c4e8614ed
Yasir <yasir.akhrouf@etu.umontpellier.fr>
2023-12-11 13:25:58 +0100

    main

--- pykorp/pykorp/wsgi.py
+++ pykorp/pykorp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projet project.
+WSGI config for pykorp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pykorp.settings')
 
 application = get_wsgi_application()
